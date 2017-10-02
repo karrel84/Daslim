66ff454745bf0609631c50c090d916f6a21dc1da
karrel <karrel84@naver.com>
2017-10-02 17:50:23 +0900

    로그인 & 회원가입

--- app/src/main/java/kr/or/fowi/daslim/daslim/presenter/MainPresenterImpl.java
+++ app/src/main/java/kr/or/fowi/daslim/daslim/presenter/MainPresenterImpl.java
@@ -1,6 +1,18 @@
 package kr.or.fowi.daslim.daslim.presenter;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.karrel.mylibrary.RLog;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import kr.or.fowi.daslim.daslim.etc.LoginManager;
 
@@ -12,10 +24,42 @@ public class MainPresenterImpl implements MainPresenter {
     private MainPresenter.View view;
     private LoginManager loginManager;
 
+    private DatabaseReference mReference;
+    private FirebaseDatabase mDatabase;
+    private Query mQuery;
+
     public MainPresenterImpl(MainPresenter.View view) {
         this.view = view;
         loginManager = LoginManager.getInstance();
 
+        // 파이어베이스
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference = mDatabase.getReference();
+
+        subscribeScheduleEvent();
+    }
+
+    private void subscribeScheduleEvent() {
+        RLog.e("subscribeScheduleEvent");
+        mQuery = mReference.child("schedule");
+        mQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> iterable = dataSnapshot.getChildren();
+                RLog.d("dataSnapshot key > " + dataSnapshot.getKey());
+                RLog.d("dataSnapshot value > " + dataSnapshot.getValue());
+
+                for (DataSnapshot snapshot : iterable) {
+                    RLog.d("key > " + snapshot.getKey());
+                    RLog.d("value > " + snapshot.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
