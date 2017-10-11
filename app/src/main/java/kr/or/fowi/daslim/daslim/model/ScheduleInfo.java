a39a960bdb44abed3a791035acdbed45dfb765e2
karrel <karrel84@naver.com>
2017-10-11 11:57:40 +0900

    예약화면에 데이터 연동 하자

---
+++ app/src/main/java/kr/or/fowi/daslim/daslim/model/ScheduleInfo.java
@@ -0,0 +1,11 @@
+package kr.or.fowi.daslim.daslim.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Rell on 2017. 10. 11..
+ */
+
+@IgnoreExtraProperties
+public class ScheduleInfo {
+}
