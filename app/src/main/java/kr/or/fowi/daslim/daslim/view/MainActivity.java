7660a1e5f68a2c990177d2b68769096a03dac7a9
karrel <karrel84@naver.com>
2017-10-13 10:25:12 +0900

    메인 화면 사용하지안흔ㄴ 뷰들 제거

--- app/src/main/java/kr/or/fowi/daslim/daslim/view/MainActivity.java
+++ app/src/main/java/kr/or/fowi/daslim/daslim/view/MainActivity.java
@@ -42,16 +42,13 @@ public class MainActivity extends BaseActivity implements MainPresenter.View {
         super.onLoadOnce();
 
         setupToolbar();
-        setupFab();
-        setupDrawerLayout();
-        setupNavigationView();
         setupViewPager();
     }
 
     private void setupViewPager() {
         adapter = new SchedulePagerAdapter(getSupportFragmentManager());
-        binding.appBarMain.contentMain.viewPager.setAdapter(adapter);
-        binding.appBarMain.contentMain.tabLayout.setupWithViewPager(binding.appBarMain.contentMain.viewPager);
+        binding.viewPager.setAdapter(adapter);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
 
 
         // TODO: 2017. 10. 12. 데이터 추가
@@ -72,78 +69,8 @@ public class MainActivity extends BaseActivity implements MainPresenter.View {
         presenter.checkLogined();
     }
 
-    private void setupNavigationView() {
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(item -> {
-            int id = item.getItemId();
-
-            if (id == R.id.nav_camera) {
-                // Handle the camera action
-            } else if (id == R.id.nav_gallery) {
-
-            } else if (id == R.id.nav_slideshow) {
-
-            } else if (id == R.id.nav_manage) {
-
-            } else if (id == R.id.nav_share) {
-
-            } else if (id == R.id.nav_send) {
-
-            }
-
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
-            return false;
-        });
-    }
-
-    private void setupDrawerLayout() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, binding.appBarMain.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-    }
-
-    private void setupFab() {
-        binding.appBarMain.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
-    }
-
     private void setupToolbar() {
-        setSupportActionBar(binding.appBarMain.toolbar);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        setSupportActionBar(binding.toolbar);
     }
 
     @Override
