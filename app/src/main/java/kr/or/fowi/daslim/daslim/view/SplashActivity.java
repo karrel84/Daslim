34fd71d894e85f09ca956271d2473251f638b15f
karrel <karrel84@naver.com>
2017-10-31 14:27:45 +0900

    스플래시  - 콜드 스타드로 인해 스플래시가 검게되었다가 시작하는 현상 보완

--- app/src/main/java/kr/or/fowi/daslim/daslim/view/SplashActivity.java
+++ app/src/main/java/kr/or/fowi/daslim/daslim/view/SplashActivity.java
@@ -15,6 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
         }, 2000);
     }
 }
